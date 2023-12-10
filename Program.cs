9b4f620e15f9e2ee9a7d5c637d1a264671b05ee7
Vini Mortinho <vinimortinho@gmail.com>
2023-12-10 20:52:55 -0300

    Base de Projeto

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System.ComponentModel;
+using ExemplosFundamentos.models;
+
+//USANDO CLASSE MATH PARA CALCULAR RAIZ QUADRADA
+Calculadora calc = new Calculadora();
+calc.RaizQuadrada(9);
+
+
+
+
+
+
+
+
+
+//FUNÇÃO INCREMENTO
+/* int numeroIncrenemento = 10 ;
+Console.WriteLine(numeroIncrenemento);
+Console.WriteLine("incrementando o 10");
+//numero = numero + 1;
+numeroIncrenemento++;
+Console.WriteLine(numeroIncrenemento); */
+
+//FUNÇÃO DECREMENTO
+/* int numeroDedremento = 20;
+Console.WriteLine(numeroDedremento);
+Console.WriteLine("decrementando o 20");
+//numero = numero - 1;
+numeroDedremento--;
+Console.WriteLine(numeroDedremento); */
+
+
+
+
+
+
+
+
+
+
+
+
+//FUNÇÃO TRIGONOMETRICA
+/* Calculadora calc = new Calculadora();
+
+calc.Seno(30);
+calc.Coseno(30);
+calc.Tangente(30); */
+
+
+
+
+
+
+
+
+
+
+
+
+
+//FUNÇÃO POTENCIA
+//calc.Potencia(3,3);
+
+
+
+
+
+
+
+
+/* Calculadora calc = new Calculadora();
+calc.Somar(10,30);
+calc.Subtrair(10,50);
+calc.Multiplicar(15,45);
+calc.Dividir(2,2);
+
+ */
